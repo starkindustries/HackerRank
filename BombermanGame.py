f4356f2b7979dedae25ffc92c5dfd92caeea97d1
Zion Perez <zionperez@MacBook-Pro.local>
2019-11-14 01:33:24 +1000

    Implement partial solution to bomberman problem. Need to optimize for n = 10^9.

--- BombermanGame.py
+++ BombermanGame.py
@@ -2,91 +2,99 @@
 
 def gridsMatch(g1, g2):
     for row in range(len(g1)):
-        if not g1[row] == g2[row]:
-            return False
+        for col in range(len(g1[0])):
+            if not g1[row][col] == g2[row][col]:
+                print(f"Test failed at: [{row}][{col}]")
+                return False
     return True
 
 def printGrid(grid):
-    for row in grid:
-        print(row)
+    fileName = "output.txt"
+    with open(fileName, 'w') as file_handle:
+        for row in grid:
+            print(row, file=file_handle)
 
 def printGrids(g1, g2):
     for row in range(len(g1)):
         print(f"{g1[row]} {g2[row]}")
 
-def bomberman(n, grid):    
-    # for i in range(n):
-    # initial state
-    print("\nInitial:")
-    printGrid(grid)
-    if n % 4 == 1:
-        return grid    
+def bomberman(n, grid):
+    # 1 second, do nothing
+    if n == 1:    
+        return grid
 
-    # fill with bombs
-    height = len(grid)
-    width = len(grid[0])
-    bombsFull = ["O" * width]*height
-
-    print("\nBombs:")
-    printGrid(bombsFull)
-    if n % 4 == 2:
-        return bombsFull
-
-    # detonate bombs from initial state
-    firstDet = [row for row in bombsFull]
-    for row in range(height):
-        for col in range(width):
-            if grid[row][col] == "O":
-                # explode locations to left and right
-                x1 = (col - 1) if col > 1 else col
-                x2 = (col + 1) if col < (width - 1) else col
-                firstDet[row] = firstDet[row][:x1] + ("." * (x2-x1+1)) + firstDet[row][x2 + 1:]
-                # explode locations above and below
-                if row > 0:
-                    firstDet[row - 1] = firstDet[row - 1][:col] + "." + firstDet[row-1][col+1:]
-                if row < (height - 1):
-                    firstDet[row + 1] = firstDet[row + 1][:col] + "." + firstDet[row + 1][col+1:]
+    height, width = len(grid), len(grid[0])
     
-    print("\nFirst Det:")
-    printGrid(firstDet) 
-    if n % 4 == 3:        
-        return firstDet        
-
-    print("\nBombs:")
-    printGrid(bombsFull)
-    if n % 4 == 0:
-        return bombsFull     
+    # store bomb states with key: value of state: order
+    bombStates = {}
         
-    # bomber fills bombs again: return full state
-    # second det happens: return original state
-    # etc
+    lastState = None    
+    for i in range(2, n + 1):     
+        # 2 seconds, fill bombs
+        if i % 2 == 0:            
+            bombs = ["O" * width] * height
+            lastState = bombs
+            continue           
+        else: # 3 seconds, detonate bombs     
+            firstDet = [row for row in bombs]
+            for row in range(height):
+                for col in range(width):
+                    if grid[row][col] == "O":
+                        # explode locations to left and right
+                        x1 = (col - 1) if col > 0 else col
+                        x2 = (col + 1) if col < (width - 1) else col
+                        firstDet[row] = firstDet[row][:x1] + ("." * (x2-x1+1)) + firstDet[row][x2 + 1:]
+                        # explode locations above and below
+                        if row > 0:
+                            firstDet[row - 1] = firstDet[row - 1][:col] + "." + firstDet[row-1][col+1:]
+                        if row < (height - 1):
+                            firstDet[row + 1] = firstDet[row + 1][:col] + "." + firstDet[row + 1][col+1:]            
+            lastState = firstDet
+            grid = [x for x in firstDet]
+        # Now repeat
+    return lastState
 
 
 # main
-inputString = """
-6 7 4
-.......
-...O.O.
-....O..
-..O....
-OO...OO
-OO.O...
-"""
-expectedString = """
-.......
-...O.O.
-...OO..
-..OOOO.
-OOOOOOO
-OOOOOOO
-"""
-lines = [line.strip() for line in inputString.splitlines() if not line.strip() == ""]
-temp = lines.pop(0).split()
-r, c, n = temp[0], temp[1], int(temp[2])
+# inputString = """
+# 6 7 5
+# .......
+# ...O.O.
+# ....O..
+# ..O....
+# OO...OO
+# OO.O...
+# """
+# expectedString = """
+# .......
+# ...O.O.
+# ...OO..
+# ..OOOO.
+# OOOOOOO
+# OOOOOOO
+# """
+# lines = [line.strip() for line in inputString.splitlines() if not line.strip() == ""]
+# expectedGrid = [line.strip() for line in expectedString.splitlines() if not line.strip() == ""]
+
+# Input file
+fileName = "./BombermanTestInput.txt"
+lines = []
+with open(fileName, 'r') as file_handle:
+    for line in file_handle:        
+        lines.append(line.strip())
+
+n = int(lines.pop(0).split()[2])
 grid = bomberman(n, lines)
-expectedGrid = [line.strip() for line in expectedString.splitlines() if not line.strip() == ""]
+
+# Expected Output file
+fileName = "./BombermanTestOutput.txt"
+expectedGrid = []
+with open(fileName, 'r') as file_handle:
+    for line in file_handle:        
+        expectedGrid.append(line.strip())
+
 if gridsMatch(grid, expectedGrid):
     print("SUCCESS!")
 else:
-    print("TEST FAILED!")
-    printGrids(grid, expectedGrid)
+    print("TEST FAILED!")    
+# printGrid(grid)
