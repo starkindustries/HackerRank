f1d3457fb04d474f4cedbbdcf3e01114969c4bdf
Zion Perez <zionperez@MacBook-Pro.local>
2019-11-20 02:39:47 +1000

    Improved performance. Tests 0-7 and 15 pass successfully. Still need to optimize further.

--- TwoTwo/TwoTwo.py
+++ TwoTwo/TwoTwo.py
@@ -1,11 +1,8 @@
 import math 
 
-TESTCASE1 = True
+TESTNUMBER = 1
 
-powersTwoMap = {}
-lastTwoDigits = {}
-lastThreeDigits = {}
-lastFourDigits = {}
+powersTwoMap = { 1 : 0 }
 def buildPowersMap():
     # build powersTwoMap
     x = 800
@@ -14,14 +11,6 @@ def buildPowersMap():
         temp *= 2    
         powersTwoMap[temp] = i
         # print(f"{str(temp)[:50]} {i} {len(str(temp))}")
-
-    # build maps
-    for value, _ in powersTwoMap.items():
-        # print(f"{str(value)[-2:]} {power}")        
-        lastTwoDigits[value % 100] = True
-        lastThreeDigits[value % 1000] = True
-        lastFourDigits[value % 10000] = True
-    # print(lastFourDigits)    
 
 # https://www.hackerrank.com/challenges/two-two/problem
 def strength(subString):
@@ -36,27 +25,15 @@ def isPowerOfTwo(n):
 maxLen = 241
 def twotwo(a):
     twoCount = 0
-    for i in range(len(a) - 1, -1, -1):
-        if int(a[i]) % 2 != 0 or int(a[i]) == 0:
-            continue        
-        maxGroupLength = i + 2 if (i + 2) < 241 else 241      
-        for groupLength in range(1, maxGroupLength):
-            if groupLength > 1 and int(a[i - 1: i + 1]) not in lastTwoDigits:
-                break
-            if groupLength > 2 and int(a[i - 2: i + 1]) not in lastThreeDigits:
-                break
-            if groupLength > 3 and int(a[i - 3: i + 1]) not in lastFourDigits:
-                break
-            group = a[i - groupLength + 1: i + 1]
-            # print(f"group: ..{group[-50:]} {groupLength} {i}")
-            if isPowerOfTwo(strength(group)):                
-                twoCount += 1
+    for value in powersTwoMap.keys():
+        if str(value) in a:
+            twoCount += a.count(str(value))
     return twoCount
     
 # main
 testCases = []
 
-if TESTCASE1:
+if TESTNUMBER == 0:
     testCases = [
         ("2222222", 7),    
         ("24256", 4),
@@ -66,13 +43,15 @@ if TESTCASE1:
         ("2121212121212121", 8)
     ]
 
-if not TESTCASE1:
+if TESTNUMBER > 0:
     testCases = []
     filename = "./TwoTwo1.txt"
+    if TESTNUMBER == 2:
+        filename = "./TwoTwo2.txt"
     lines = []
     with open(filename, 'r') as fileHandle:
         for line in fileHandle:
-            lines.append(line.strip())
+            lines.append(line.strip())    
     n = int(lines.pop(0))
     for i in range(n):
         testCases.append((lines[i], int(lines[n + i + 1])))
@@ -80,4 +59,7 @@ if not TESTCASE1:
 buildPowersMap()
 for t in testCases:
     result = twotwo(t[0])
-    print(f"result: {result}")
+    if result == t[1]:
+        print(f"SUCCESS: {result}")
+    else: 
+        print(f"FAIL: {result}. Expected {t[1]}")
