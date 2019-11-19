55e70c438174fecae9766b0e750fec1dbc4cff45
Zion Perez <zionperez@MacBook-Pro.local>
2019-11-20 01:30:04 +1000

    Improved twotwo solution. Stil need to optimize further.

--- TwoTwo/TwoTwo.py
+++ TwoTwo/TwoTwo.py
@@ -1,5 +1,24 @@
 import math 
 
+TESTCASE1 = True
+
+powersTwoMap = {}
+lastTwoDigits = {}
+def buildPowersMap():
+    # build powersTwoMap
+    x = 800
+    temp = 1
+    for i in range(1, x + 1):
+        temp *= 2    
+        powersTwoMap[temp] = i
+        # print(f"{str(temp)[:50]} {i} {len(str(temp))}")
+
+    # build lastTwoDigits map
+    for value, _ in powersTwoMap.items():
+        # print(f"{str(value)[-2:]} {power}")
+        if value % 100 > 9:
+            lastTwoDigits[value % 100] = True
+
 # https://www.hackerrank.com/challenges/two-two/problem
 def strength(subString):
     if int(subString[0]) == 0:
@@ -7,64 +26,50 @@ def strength(subString):
     return int(subString)
 
 def isPowerOfTwo(n):    
-    return math.log2(n).is_integer()    
+    return (n in powersTwoMap)
 
+# Output the total number of strengths of the form 2^x such that 0 <= x <= 800.
+maxLen = 241
 def twotwo(a):
     twoCount = 0
-    groupMap = {}
-    # loop through all groups of i-j or build a groups
-    # loop through all possible group lengths from 1 to len(a)
-    for i in range(1, len(a) + 1):
-        # loop through all groups of length i
-        for j in range(0, len(a) - i + 1):
-            # this is a group
-            group = a[j:j + i]
-            groupMap[group] = groupMap.get(group, 0) + 1
-            # print(f"{j} {j + i} {group}")    
-    print("DONE")
-    return 5
-    # for each group, find the group's strength
-    for group, count in groupMap.items():
-        # determine if strength is a power of 2
-        if isPowerOfTwo(strength(group)):
-            # if is power of 2, increment count
-            twoCount += count
+    for i in range(len(a) - 1, -1, -1):
+        if int(a[i]) % 2 != 0 or int(a[i]) == 0:
+            continue        
+        maxGroupLength = i + 2 if (i + 2) < 241 else 241      
+        for groupLength in range(1, maxGroupLength):
+            if groupLength > 1 and int(a[i - 1: i + 1]) not in lastTwoDigits:
+                break
+            group = a[i - groupLength + 1: i + 1]
+            # print(f"group: {group} {groupLength} {i}")
+            if isPowerOfTwo(strength(group)):                
+                twoCount += 1
     return twoCount
     
 # main
-"""
-testCases = [
-    ("2222222", 7),    
-    ("24256", 4),
-    ("65536", 1),
-    ("023223", 4),
-    ("33579", 0),
-]
-"""
-
 testCases = []
-filename = "./TwoTwo1.txt"
-lines = []
-with open(filename, 'r') as fileHandle:
-    for line in fileHandle:
-        lines.append(line)
-n = int(lines.pop(0))
-for i in range(n):
-    testCases.append((lines[i], int(lines[n + i + 1])))
 
+if TESTCASE1:
+    testCases = [
+        ("2222222", 7),    
+        ("24256", 4),
+        ("65536", 1),
+        ("023223", 4),
+        ("33579", 0),
+        ("2121212121212121", 8)
+    ]
+
+if not TESTCASE1:
+    testCases = []
+    filename = "./TwoTwo1.txt"
+    lines = []
+    with open(filename, 'r') as fileHandle:
+        for line in fileHandle:
+            lines.append(line.strip())
+    n = int(lines.pop(0))
+    for i in range(n):
+        testCases.append((lines[i], int(lines[n + i + 1])))
+
+buildPowersMap()
 for t in testCases:
     result = twotwo(t[0])
     print(f"result: {result}")
-
-
-# up to len 10^5 = 100,000
-"""
-x = 332193
-powersOfTwo = [2]
-for i in range(1, x + 1):
-    temp = powersOfTwo[i - 1]
-    powersOfTwo.append(2 * temp)
-    isPow2 = isPowerOfTwo(temp)
-    print(f"{i} {str(temp)[:50]}.. {isPow2}")
-print(f"{powersOfTwo}")
-"""
