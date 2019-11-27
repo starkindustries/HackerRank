e03ab2822d82d27861ce96348d97b763460bfd54
Zion Perez <zionperez@gmail.com>
2019-11-27 19:28:09 +0900

    Add placeholder file for algo challenges.

---
+++ Playgrounds/helloworld.py
@@ -0,0 +1,12 @@
+
+
+# Read a file
+# fileName = "./MorganAndString7.txt"
+# lines = []
+# with open(fileName, 'r') as file_handle:
+#     for line in file_handle:
+#         lines.append(line)
+
+
+# main
+print("test")
